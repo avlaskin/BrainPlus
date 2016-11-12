a82fe8c577e4d85d9f5003762d27ca50ae4db03d
Alexey Vlaskin <alex@avlaskin.com>
2016-11-12 13:11:19 +1100

    Improve tests to have a good coverage for the interpreter

--- Tests/BrainPlusLanguageInterpreterTests/bfpvmtests.swift
+++ Tests/BrainPlusLanguageInterpreterTests/bfpvmtests.swift
@@ -1,250 +1,13 @@
+//  Copyright © 2016 Alexey Vlaskin. All rights reserved.
+
 @testable import BrainPlusLanguageInterpreter
 
 import XCTest
 import Foundation
 
-/*
-
-
-public func test3() -> Bool {
-    let program = ">>++"
-    let instructionSet:[InterpreterCommand] = [IncreaseData(),DecreaseData(),DecreaseData(),DecreasePointer(),IncreasePointer()]
-    let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
-    let vm = BrainPlusVM()
-    let (res, passed) = i.run(brainfvm: vm)
-    if !passed || res.characters.count > 0 {
-        return false
-    }
-    if vm.dataPointer != 2 {
-        return false
-    }
-    if vm.getPointerValue() != 2 {
-        print("Error : expected 2 == \(vm.getPointerValue())")
-        print("\(vm.description())")
-        return false
-    }
-    return true
-}
-public func test4() -> Bool {
-    let program = ">>+-"
-    let instructionSet:[InterpreterCommand] = [IncreaseData(),DecreaseData(),DecreasePointer(),IncreasePointer()]
-    let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
-    let vm = BrainPlusVM()
-    let (res, passed) = i.run(brainfvm: vm)
-    if !passed || res.characters.count > 0 {
-        return false
-    }
-    if vm.dataPointer != 2 {
-        return false
-    }
-    if vm.getPointerValue() != 0 {
-        print("Error : expected 2 == \(vm.getPointerValue())")
-        print("\(vm.description())")
-        return false
-    }
-    return true
-}
-
-public func test5() -> Bool {
-    let program = ">>+--"
-    let instructionSet:[InterpreterCommand] = [IncreaseData(),DecreaseData(),DecreasePointer(),IncreasePointer()]
-    let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
-    let vm = BrainPlusVM()
-    let (res, passed) = i.run(brainfvm: vm)
-    if !passed || res.characters.count > 0 {
-        return false
-    }
-    if vm.dataPointer != 2 {
-        return false
-    }
-    if vm.getPointerValue() != 255 {
-        print("Error : expected 255 == \(vm.getPointerValue())")
-        print("\(vm.description())")
-        return false
-    }
-    return true
-}
-
-public func test6() -> Bool {
-    let program = ">>+--."
-    let instructionSet:[InterpreterCommand] = [
-        IncreaseData(),
-        DecreaseData(),
-        DecreasePointer(),
-        IncreasePointer(),
-        Output()]
-    let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
-    let vm = BrainPlusVM()
-    let (res, passed) = i.run(brainfvm: vm)
-    if !passed {
-        return false
-    }
-    if res.characters.count != 1 {
-        return false
-    }
-    if vm.dataPointer != 2 {
-        return false
-    }
-    if vm.getPointerValue() != 255 {
-        print("Error : expected 255 == \(vm.getPointerValue())")
-        print("\(vm.description())")
-        return false
-    }
-    return true
-}
-
-public func test7() -> Bool {
-    //This should be the test for input, but I will skip it for now
-    let program = ">>+--."
-    let instructionSet:[InterpreterCommand] = [
-        IncreaseData(),
-        DecreaseData(),
-        DecreasePointer(),
-        IncreasePointer(),
-        Output()]
-    let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
-    let vm = BrainPlusVM()
-    let (res, passed) = i.run(brainfvm: vm)
-    if !passed {
-        return false
-    }
-    if res.characters.count != 1 {
-        return false
-    }
-    if vm.dataPointer != 2 {
-        return false
-    }
-    if vm.getPointerValue() != 255 {
-        print("Error : expected 255 == \(vm.getPointerValue())")
-        print("\(vm.description())")
-        return false
-    }
-    return true
-}
-
-public func test8() -> Bool {
-    //This tests invalid brackets
-    let program = ">>+--["
-    let instructionSet:[InterpreterCommand] = [
-        IncreaseData(),
-        DecreaseData(),
-        DecreasePointer(),
-        IncreasePointer(),
-        Output()]
-    let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
-    let vm = BrainPlusVM()
-    let (res, passed) = i.run(brainfvm: vm)
-    if passed {
-        return false
-    }
-    if res.characters.count > 0 {
-        print("Error: \(res)")
-    } else {
-        return false
-    }
-    // it should fail
-    return true
-}
-
-public func test9() -> Bool {
-    //This tests invalid brackets
-    let program = "+++++++++[.-]"
-    let instructionSet:[InterpreterCommand] = [
-        IncreaseData(),
-        DecreaseData(),
-        DecreasePointer(),
-        IncreasePointer(),
-        Output(),
-        LoopStart(),
-        LoopEnd()]
-    let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
-    let vm = BrainPlusVM()
-    let (res, passed) = i.run(brainfvm: vm)
-    if !passed {
-        return false
-    }
-    if res.characters.count < 9 {
-        return false
-    }
-    return true
-}
-
-public func test10() -> Bool {
-    //This tests should show hello world
-    let expectedResult = "Hello world!"
-    let program = "--[>--->->->++>-<<<<<-------]>--.>---------.>--..+++.>----.>+++++++++.<<.+++.------.<-.>>+."
-    let instructionSet:[InterpreterCommand] = [
-        IncreaseData(),
-        DecreaseData(),
-        DecreasePointer(),
-        IncreasePointer(),
-        Output(),
-        LoopStart(),
-        LoopEnd()]
-    let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
-    let vm = BrainPlusVM()
-    let (res, passed) = i.run(brainfvm: vm)
-    if !passed {
-        return false
-    }
-    if res != expectedResult {
-        return false
-    }
-    // it should fail
-    return true
-}
-
-public func test11() -> Bool {
-    //This tests should show hello world
-    let expectedResult = "Hello World!\n"
-    let program = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
-    let instructionSet:[InterpreterCommand] = [
-        IncreaseData(),
-        DecreaseData(),
-        DecreasePointer(),
-        IncreasePointer(),
-        Output(),
-        LoopStart(),
-        LoopEnd()]
-    let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
-    let vm = BrainPlusVM()
-    let (res, passed) = i.run(brainfvm: vm)
-    if !passed {
-        return false
-    }
-    if res != expectedResult {
-        return false
-    }
-    return true
-}
-
-public func test12() -> Bool {
-    let expectedResult = "Hello World!\n"
-    //This is a slightly more complex variant that often triggers interpreter bugs
-    let program = ">++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->+++..+++.>-.<<+[>[+>+]>>]<--------------.>>.+++.------.--------.>+.>+."
-    let instructionSet:[InterpreterCommand] = [
-        IncreaseData(),
-        DecreaseData(),
-        DecreasePointer(),
-        IncreasePointer(),
-        Output(),
-        LoopStart(),
-        LoopEnd()]
-    let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
-    let vm = BrainPlusVM()
-    let (res, passed) = i.run(brainfvm: vm)
-    if !passed {
-        return false
-    }
-    if res != expectedResult {
-        return false
-    }
-    // it should fail
-    return true
-}
-*/
 class BPTests: XCTestCase {
-    func testBrainPlusInterpreterFirst() {
+    
+    func testBrainPlusInterpreter1() {
         var result = false
         let program = ">>"
         let instructionSet:[InterpreterCommand] = [IncreaseData(),DecreaseData()]
@@ -263,7 +26,7 @@ class BPTests: XCTestCase {
         XCTAssertTrue(result, "Test move pointer failed #1")
     }
     
-    func testBrainPlusInterpreterSecond() {
+    func testBrainPlusInterpreter2() {
         var result = false
         let program = ">>++"
         let instructionSet:[InterpreterCommand] = [IncreaseData(),DecreaseData(),DecreaseData(),DecreasePointer(),IncreasePointer()]
@@ -284,5 +47,163 @@ class BPTests: XCTestCase {
             result = true
         }
         XCTAssertTrue(result, "Test move pointer failed #2")
+    }
+    
+    func testBrainPlusInterpreter3() {
+        let program = ">>++"
+        let instructionSet:[InterpreterCommand] = [IncreaseData(),DecreaseData(),DecreaseData(),DecreasePointer(),IncreasePointer()]
+        let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
+        let vm = BrainPlusVM()
+        let (res, passed) = i.run(brainfvm: vm)
+        XCTAssertTrue(passed || res.characters.count == 0, "Test move and increase pointer failed $3")
+        XCTAssertTrue(vm.dataPointer == 2,"Test move and increase pointer failed")
+        XCTAssertTrue(vm.getPointerValue() == 2, "Error : expected 2")
+    }
+    
+    func testBrainPlusInterpreter4() {
+        let program = ">>+-"
+        let instructionSet:[InterpreterCommand] = [IncreaseData(),DecreaseData(),DecreasePointer(),IncreasePointer()]
+        let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
+        let vm = BrainPlusVM()
+        let (res, passed) = i.run(brainfvm: vm)
+        XCTAssertTrue(passed || res.characters.count == 0, "Test move and increase/decrease pointer failed #4")
+        XCTAssertTrue(vm.dataPointer == 2,"Test move pointer failed #4")
+        XCTAssertTrue(vm.getPointerValue() == 0, "Error : expected 0 #4")
+    }
+    
+    func testBrainPlusInterpreter5() {
+        let program = ">>+--"
+        let instructionSet:[InterpreterCommand] = [IncreaseData(),DecreaseData(),DecreasePointer(),IncreasePointer()]
+        let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
+        let vm = BrainPlusVM()
+        let (res, passed) = i.run(brainfvm: vm)
+        XCTAssertTrue(passed || res.characters.count == 0, "Test move and increase/decrease pointer failed #5")
+        XCTAssertTrue(vm.dataPointer == 2,"Test move pointer failed #5")
+        XCTAssertTrue(vm.getPointerValue() == 255, "Overflow Error : expected 255 #5")
+    }
+    
+    func testBrainPlusInterpreter6() {
+        let program = ">>+--."
+        let instructionSet:[InterpreterCommand] = [
+            IncreaseData(),
+            DecreaseData(),
+            DecreasePointer(),
+            IncreasePointer(),
+            Output()]
+        let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
+        let vm = BrainPlusVM()
+        let (res, passed) = i.run(brainfvm: vm)
+        XCTAssertTrue(passed || res.characters.count == 1, "Test failed #6")
+        XCTAssertTrue(vm.dataPointer == 2,"Test move pointer failed #6")
+        XCTAssertTrue(vm.getPointerValue() == 255, "Overflow Error : expected 255 #6")
+    }
+    
+    func testBrainPlusInterpreter7() {
+        //This should be the test for input, but I will skip it for now
+        let program = ">>+--."
+        let instructionSet:[InterpreterCommand] = [
+            IncreaseData(),
+            DecreaseData(),
+            DecreasePointer(),
+            IncreasePointer(),
+            //Input(),
+            Output()]
+        let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
+        let vm = BrainPlusVM()
+        let (res, passed) = i.run(brainfvm: vm)
+        XCTAssertTrue(passed || res.characters.count == 1, "Test failed #7")
+        XCTAssertTrue(vm.dataPointer == 2,"Test move pointer failed #7")
+        XCTAssertTrue(vm.getPointerValue() == 255, "Overflow Error : expected 255 #7")
+    }
+    
+    func testBrainPlusInterpreter8() {
+        //This tests invalid brackets
+        let program = ">>+--["
+        let instructionSet:[InterpreterCommand] = [
+            IncreaseData(),
+            DecreaseData(),
+            DecreasePointer(),
+            IncreasePointer(),
+            Output()]
+        let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
+        let vm = BrainPlusVM()
+        let (res, passed) = i.run(brainfvm: vm)
+        XCTAssertFalse(passed, "Test failed #8")
+        // Res should have an error description
+        XCTAssertTrue(res.characters.count > 0, "Test failed #8")
+    }
+
+    func testBrainPlusInterpreter9() {
+        //This tests invalid brackets
+        let program = "+++++++++[.-]"
+        let instructionSet:[InterpreterCommand] = [
+            IncreaseData(),
+            DecreaseData(),
+            DecreasePointer(),
+            IncreasePointer(),
+            Output(),
+            LoopStart(),
+            LoopEnd()]
+        let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
+        let vm = BrainPlusVM()
+        let (res, passed) = i.run(brainfvm: vm)
+        XCTAssertTrue(passed || res.characters.count > 8, "Test failed #9")
+    }
+    
+    func testBrainPlusInterpreter10() {
+        //This tests should show hello world
+        let expectedResult = "Hello world!"
+        let program = "--[>--->->->++>-<<<<<-------]>--.>---------.>--..+++.>----.>+++++++++.<<.+++.------.<-.>>+."
+        let instructionSet:[InterpreterCommand] = [
+            IncreaseData(),
+            DecreaseData(),
+            DecreasePointer(),
+            IncreasePointer(),
+            Output(),
+            LoopStart(),
+            LoopEnd()]
+        let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
+        let vm = BrainPlusVM()
+        let (res, passed) = i.run(brainfvm: vm)
+        XCTAssertTrue(passed, "Test program failed #10")
+        XCTAssertEqual(res, expectedResult, "Test program Hello World - failed #10")
+    }
+    
+    func testBrainPlusInterpreter11() {
+        //This tests should show hello world
+        let expectedResult = "Hello World!\n"
+        let program = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
+        let instructionSet:[InterpreterCommand] = [
+            IncreaseData(),
+            DecreaseData(),
+            DecreasePointer(),
+            IncreasePointer(),
+            Output(),
+            LoopStart(),
+            LoopEnd()]
+        let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
+        let vm = BrainPlusVM()
+        let (res, passed) = i.run(brainfvm: vm)
+        XCTAssertTrue(passed, "Test program failed #11")
+        XCTAssertEqual(res, expectedResult, "Test program Hello World - failed #11")
+    }
+    
+    func testBrainPlusInterpreter12() {
+        //This is a slightly more complex variant that often triggers interpreter bugs
+        let expectedResult = "Hello World!\n"
+        let program = ">++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->+++..+++.>-.<<+[>[+>+]>>]<--------------.>>.+++.------.--------.>+.>+."
+        let instructionSet:[InterpreterCommand] = [
+            IncreaseData(),
+            DecreaseData(),
+            DecreasePointer(),
+            IncreasePointer(),
+            Output(),
+            LoopStart(),
+            LoopEnd()]
+        let i = BrainPlusInterpreter(program: program, instructions:instructionSet)
+        let vm = BrainPlusVM()
+        let (res, passed) = i.run(brainfvm: vm)
+        XCTAssertTrue(passed, "Test program failed #12")
+        XCTAssertEqual(res, expectedResult, "Test program Hello World - failed #12")
     }
 }
